6c14a5de5f880005fd9b80b2cee2569b1743e529
Paulo Brezolin de Oliveira <112288627+PauloBrezolin@users.noreply.github.com>
2023-05-24 01:43:45 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -3,7 +3,7 @@ $host = 'localhost';
 $port = '5432';
 $dbname = 'produtos';
 $user = 'postgres';
-$password = '2022';
+$password = '1234';
 
 $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
